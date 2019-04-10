0b4c75d7df5d58c918b5e8766d56e920477e690b
PorkStew <45571466+PorkStew@users.noreply.github.com>
2019-04-10 10:21:57 +0200

    Uploading of files

---
+++ app/src/main/java/com/example/lost/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.lost;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        fragmentTransaction.add(R.id.fragment_container, new LoginFragment());
+        fragmentTransaction.commit();
+    }
+}
