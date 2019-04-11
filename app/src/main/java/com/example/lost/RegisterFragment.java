9717994cd59b3e8a86a6c06349a73cc65d4efe83
PorkStew <45571466+PorkStew@users.noreply.github.com>
2019-04-11 10:17:04 +0200

    firebase upload working

--- app/src/main/java/com/example/lost/RegisterFragment.java
+++ app/src/main/java/com/example/lost/RegisterFragment.java
@@ -5,9 +5,11 @@ import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
@@ -30,8 +32,31 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("its working yeah!!!!");
+
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        Button submitB = (Button)view.findViewById(R.id.SubmitB);
+
+        submitB.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+                users user = new users();
+
+
+            }
+
+
+        });
+        return view;
+
+    }
 
     }
 
