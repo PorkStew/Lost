5da258775d42d37a614ca1d7a144fb561113b155
PorkStew <45571466+PorkStew@users.noreply.github.com>
2019-04-16 21:25:28 +0200

    added navigation drawer more issues tho

--- app/src/main/java/com/example/lost/HomeFragment.java
+++ app/src/main/java/com/example/lost/HomeFragment.java
@@ -11,7 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
 import com.mapbox.android.core.location.LocationEngine;
 import com.mapbox.android.core.location.LocationEngineProvider;
 import com.mapbox.android.core.permissions.PermissionsListener;
